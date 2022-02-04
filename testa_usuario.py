5f19d98d3393153370e4094da5cb063803655289
profnelsonseixas <74307897+profnelsonseixas@users.noreply.github.com>
2022-02-04 15:11:52 -0300

    Add files via upload

---
+++ testa_usuario.py
@@ -0,0 +1,42 @@
+"""
+Programa 2
+Descrição: Este programa pede a entrada de dados pelo usuário
+até que o dado seja do tipo adequado.
+Autor: Nelson S. dos Santos
+Versão: 0.0.1
+"""
+
+# Entrada
+
+# Atribuição de variáveis: usuários do sistema
+
+usuario1 = "nelson"
+usuario2 = "joao"
+usuario3 = "juan"
+usuario4 = "nathan"
+
+while True:
+    usuario = input("\nQual o seu nome de usuário?")
+    if usuario == usuario1:
+        print(f"\nBenvindo ao sistema, {usuario1}")
+        break
+    elif usuario == usuario2:
+        print(f"\nBenvindo ao sistema, {usuario2}")
+        break
+    elif usuario == usuario2:
+        print(f"\nBenvindo ao sistema, {usuario3}")
+        break
+    elif usuario == usuario2:
+        print(f"\nBenvindo ao sistema, {usuario4}")
+        break
+    else:
+        print("\nUsuário inválido.  Tente novamente!")
+        
+
+
+# Processamento
+
+
+
+# Saída
+
