087c8b6cc4980fd6b0df821f5c397a8057b64622
profnelsonseixas <74307897+profnelsonseixas@users.noreply.github.com>
2023-07-07 16:34:47 -0300

    Create soma_sequencial.py

---
+++ 2023-1/aula07072023/soma/soma_sequencial.py
@@ -0,0 +1,31 @@
+# Soma sequencial
+
+# Atribuição de variáveis
+
+numero_1 = 0
+
+numero_2 = 0
+
+soma = 0
+
+
+
+
+# Entrada de dados
+
+numero_1 = float(input("Digite o primeiro número: "))
+
+numero_2 = float(input("Digite o segundo número: "))
+
+
+# Processamento de dados
+
+soma = numero_1 + numero_2
+
+
+
+# Saída de dados
+
+print(f"A soma do numero {numero_1} com o número {numero_2} é igual a {soma}.")
+
+
