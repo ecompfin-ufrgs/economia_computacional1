749feb91a0a4bce04374c6696810da4190fa40ab
profnelsonseixas <74307897+profnelsonseixas@users.noreply.github.com>
2025-04-02 09:29:52 -0300

    Create testa_adulto.py

---
+++ programacao_estruturada/testa_adulto.py
@@ -0,0 +1,14 @@
+
+# Alocação de memória
+
+idade = 0
+
+# Entrada de dados
+idade = int(input("Qual a sua idade?"))
+
+# Processamento de dados
+if idade > 18:
+    print("Oi!  Voce eh um adulto.")
+
+# Saída de dados
+print("Bom saber sua idade.")
