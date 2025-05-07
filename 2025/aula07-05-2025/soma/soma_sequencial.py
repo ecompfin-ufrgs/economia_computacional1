40f8b5f125e3d559df126c33da0b07022f0b80b4
profnelsonseixas <74307897+profnelsonseixas@users.noreply.github.com>
2025-05-07 15:11:37 -0300

    Create soma_sequencial.py

---
+++ 2025/aula07-05-2025/soma/soma_sequencial.py
@@ -0,0 +1,28 @@
+# Programa soma sequencial
+# Este programa soma dois números dados pelo usuário.
+
+# Alocação de memória
+
+numero_1 =0
+
+numero_2 = 0
+
+resultado = 0
+
+i = 0
+
+# Entrada
+
+numero_1 = float(input(f"\nDigite o número {i + 1}: "))
+
+numero_2 = float(input(f"\nDigite o número {i + 2}: "))
+
+
+# Processamento 
+
+resultado = numero_1 + numero_2
+
+
+# Saída
+
+print(f"\nA soma do número {numero_1} com o número {numero_2} é igual a {resultado}.")
