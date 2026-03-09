6e15ce99213b7b98043d6b53c36c5d5e84213e06
profnelsonseixas <74307897+profnelsonseixas@users.noreply.github.com>
2026-03-09 19:28:36 -0300

    Add files via upload

---
+++ 2026/Aula2-10-03-2026/codigos/carta_ao_interpretador.py
@@ -0,0 +1,42 @@
+"""Programa 1
+    Descrição: Este programa mostra apenas as partes
+    básicas de um programa escrito dentro do paradigma
+    sequencial.
+    Autor: Nelson Seixas dos Santos
+    Data: 10/03/2026
+    Versão: 0.0.1
+"""
+
+# Alocação de memória para as variáveis
+
+"""
+Variáveis são espaços na memória do computador
+onde podemos armazenar dados temporariamente
+ durante a execução de um programa.
+ Elas são usadas para guardar informações 
+ que podem ser manipuladas e utilizadas 
+ ao longo do código. 
+ Em Python, não é necessário declarar 
+ o tipo da variável, pois a linguagem é 
+ dinamicamente tipada. 
+ Basta atribuir um valor a uma variável 
+ para que ela seja criada.
+ """
+
+x = 0  # Exemplo de alocação de memória para a variável x
+y = 0  # Exemplo de alocação de memória para a variável y
+soma = 0  # Exemplo de alocação de memória para a variável soma
+
+# Entrada de dados
+
+x = int(input("Digite um número inteiro: "))  # Exemplo de entrada de dados para a variável x    
+
+y = int(input("Digite outro número inteiro: "))  # Exemplo de entrada de dados para a variável y
+
+# Processamento de dados
+
+soma = x + y  # Exemplo de processamento de dados: soma das variáveis x e y
+# Saída de dados
+
+print(f"A soma dos números é: {soma}")  # Exemplo de saída de dados: impressão do resultado
+
