01aeb0cc2f491a3c18259928b208a2412451239c
profnelsonseixas <74307897+profnelsonseixas@users.noreply.github.com>
2026-03-24 21:43:15 -0300

    Update calcula_media.py

--- programacao_estruturada/calcula_media.py
+++ programacao_estruturada/calcula_media.py
@@ -1,17 +1,17 @@
 #Alocacao de memoria
-soma: float = 0
-valor: float = 0
+soma: float = 0.0
+valor: float = 0.0
 i: int = 0
 num_val: int = 0
 
-# Entrada e processamento de dados
+#### Entrada e processamento de dados
 num_val = int(input("Qual o numero de valores?\n"))
 
 while i < num_val:
-    valor = float(input("Digite um numero.\n"))
+    valor = float(input(f"Digite numero {i+1}.\n"))
     soma = soma + valor
     i = i + 1
-    media = soma/numero_valores
+    media = soma/num_val
 
 # Saida de dados    
-print('A media dos numeros eh igual a:', media)  
+print('A media dos numeros eh igual a %5.2f:'%media)  
